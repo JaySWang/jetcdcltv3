807844d77b5aeef177f803b99446d8ec27c70375
Dyson <wangnjuse@gmail.com>
2016-02-29 16:19:20 +0800

    implement range watch lease and relevant tests

--- jetcdcltv3/src/main/java/com/sap/etcdcltv3/EtcdClient.java
+++ jetcdcltv3/src/main/java/com/sap/etcdcltv3/EtcdClient.java
@@ -1,24 +1,44 @@
 package com.sap.etcdcltv3;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 import com.google.protobuf.ByteString;
+
 import etcdserverpb.KVGrpc;
+import etcdserverpb.LeaseGrpc;
+import etcdserverpb.Rpc.LeaseCreateRequest;
+import etcdserverpb.Rpc.LeaseCreateResponse;
+import etcdserverpb.Rpc.LeaseKeepAliveRequest;
+import etcdserverpb.Rpc.LeaseKeepAliveResponse;
 import etcdserverpb.Rpc.PutRequest;
 import etcdserverpb.Rpc.PutResponse;
 import etcdserverpb.Rpc.RangeRequest;
 import etcdserverpb.Rpc.RangeResponse;
+import etcdserverpb.Rpc.WatchCreateRequest;
+import etcdserverpb.Rpc.WatchRequest;
+import etcdserverpb.Rpc.WatchResponse;
+import etcdserverpb.WatchGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+import storagepb.Kv.KeyValue;
 
 public class EtcdClient {
 	private static final Logger logger = Logger.getLogger(EtcdClient.class.getName());
 	private final ManagedChannel channel;
 	private final KVGrpc.KVBlockingStub blockingStub;
-
+	private final WatchGrpc.WatchStub watchStub;
+	private final LeaseGrpc.LeaseStub leaseStub;
+	private final LeaseGrpc.LeaseBlockingStub leaseBlockingStub;
+	
 	public EtcdClient(String host, int port) {
 		channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext(true).build();
 		blockingStub = KVGrpc.newBlockingStub(channel);
+		watchStub = WatchGrpc.newStub(channel);
+		leaseStub = LeaseGrpc.newStub(channel);
+		leaseBlockingStub = LeaseGrpc.newBlockingStub(channel);
 	}
 
 	public void shutdown() throws InterruptedException {
@@ -29,15 +49,103 @@ public class EtcdClient {
 		logger.info("try to get key:" + key.toStringUtf8());
 		RangeRequest request = RangeRequest.newBuilder().setKey(key).build();
 		RangeResponse response = blockingStub.range(request);
-		String result = response.getKvs(0).getValue().toStringUtf8();
+		String result = null; 
+		if(response.getKvsCount()>0){
+		result = response.getKvs(0).getValue().toStringUtf8();
+		}
 		return result;
 	}
 
-	public void put(ByteString key, ByteString value, ByteString lease) {
+	public List<String> range(ByteString key, ByteString range_end) {
+		logger.info("try to range key:" + key + "to " +range_end);
+		RangeRequest request = RangeRequest.newBuilder().setKey(key).setRangeEnd(range_end).build();
+		RangeResponse response = blockingStub.range(request);
+		List<KeyValue> kvResults = response.getKvsList();
+		List<String> results = new ArrayList<String>();
+		for(KeyValue kv:kvResults){
+			results.add(kv.getValue().toStringUtf8());
+		}
+		return results;
+	}
+	
+	public void put(ByteString key, ByteString value, long lease) {
 		logger.info("try to put key:" + key.toStringUtf8() + " value:" + value.toStringUtf8() + " lease:"
-				+ lease.toStringUtf8());
+				+ lease);
+		PutRequest request = PutRequest.newBuilder().setKey(key).setValue(value).setLease(lease).build();
+		PutResponse response = blockingStub.put(request);
+	}
+
+	public void put(ByteString key, ByteString value) {
+		logger.info("try to put key:" + key.toStringUtf8() + " value:" + value.toStringUtf8() );
 		PutRequest request = PutRequest.newBuilder().setKey(key).setValue(value).build();
 		PutResponse response = blockingStub.put(request);
 	}
+	
+	public void watchPrefix(ByteString prefix,StreamObserver<WatchResponse> responseObserver){
+		logger.info("try to watch prefix:" + prefix.toStringUtf8() );
+
+		StreamObserver<WatchRequest> requestObserver =watchStub.watch(responseObserver);
+		WatchCreateRequest createRequest = WatchCreateRequest.newBuilder().setPrefix(prefix).build();
+	    WatchRequest request = WatchRequest.newBuilder().setCreateRequest(createRequest).build();
+		requestObserver.onNext(request);
+	}
+	
+	public void watch(ByteString key,StreamObserver<WatchResponse> responseObserver){
+		logger.info("try to watch key:" + key.toStringUtf8() );
+		StreamObserver<WatchRequest> requestObserver =watchStub.watch(responseObserver);
+		WatchCreateRequest createRequest = WatchCreateRequest.newBuilder().setKey(key).build();
+	    WatchRequest request = WatchRequest.newBuilder().setCreateRequest(createRequest).build();
+		requestObserver.onNext(request);
+
+	}
+
+	public long createLease(int ttl) {
+		logger.info("try to create lease with ttl:" + ttl);
+		LeaseCreateRequest request = LeaseCreateRequest.newBuilder().setTTL(ttl).build();	
+		LeaseCreateResponse response = leaseBlockingStub.leaseCreate(request);	
+		return response.getID();
+	}
+	
+	public void keepAliveLease(final long id){
+		final StreamObserver<LeaseKeepAliveResponse> responseObserver =new StreamObserver<LeaseKeepAliveResponse>(){
+			@Override
+			public void onCompleted() {
+			}
+
+			@Override
+			public void onError(Throwable arg0) {
+			}
+
+			@Override
+			public void onNext(LeaseKeepAliveResponse response) {
+				logger.info("keep lease: "+response.getID()+" alive with ttl:" + response.getTTL());
+				try {
+					Thread.sleep(1000*response.getTTL()/2);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				StreamObserver<LeaseKeepAliveRequest> requestObserver = leaseStub.leaseKeepAlive(this);
+				LeaseKeepAliveRequest request = LeaseKeepAliveRequest.newBuilder().setID(id).build();
+				requestObserver.onNext(request);
+			}
+			
+		};
+		
+		Thread keepalive = new Thread(new Runnable(){
+			@Override
+			public void run() {
+				StreamObserver<LeaseKeepAliveRequest> requestObserver = leaseStub.leaseKeepAlive(responseObserver);
+				LeaseKeepAliveRequest request = LeaseKeepAliveRequest.newBuilder().setID(id).build();
+				requestObserver.onNext(request);				
+			}
+			
+		});
+		keepalive.start();
+
+	}
+
+	
+	
 
 }
--- jetcdcltv3/src/test/java/com/sap/etcdcltv3/EtcdClientTest.java
+++ jetcdcltv3/src/test/java/com/sap/etcdcltv3/EtcdClientTest.java
@@ -1,8 +1,14 @@
 package com.sap.etcdcltv3;
 
+import java.util.List;
+
 import org.junit.*;
 
 import com.google.protobuf.ByteString;
+import com.sap.etcd.observer.WatchStreamObserver;
+
+import etcdserverpb.Rpc.WatchResponse;
+import io.grpc.stub.StreamObserver;
 
 public class EtcdClientTest {
 	EtcdClient client;
@@ -13,31 +19,137 @@ public class EtcdClientTest {
 		int port = 2378;
 		client = new EtcdClient(host, port);
 	}
-
+ 
 	@Test
 	public void testPut() {
 		ByteString key = ByteString.copyFromUtf8("etcd");
 		ByteString value = ByteString.copyFromUtf8("nice");
-		ByteString lease = null;
-		client.put(key, value, lease);
+		client.put(key, value);
 		Assert.assertTrue(true);
-
 	}
 
 	@Test
 	public void testRange() {
-		ByteString key = ByteString.copyFromUtf8("range");
-		ByteString value = ByteString.copyFromUtf8("good");
-		ByteString lease = null;
-		client.put(key, value, lease);
+		ByteString key1 = ByteString.copyFromUtf8("range1");
+		ByteString value1 = ByteString.copyFromUtf8("good1");
+		client.put(key1, value1);
 		Assert.assertTrue(true);
 
-		String result = client.range(key);
-		Assert.assertEquals(value.toStringUtf8(), result);
+		String result = client.range(key1);
+		Assert.assertEquals(value1.toStringUtf8(), result);
+		
+		ByteString key2 = ByteString.copyFromUtf8("range2");
+		ByteString value2 = ByteString.copyFromUtf8("good2");
+		ByteString key3 = ByteString.copyFromUtf8("range3");
+		ByteString value3 = ByteString.copyFromUtf8("good3");
+		
+		client.put(key2, value2);
+		client.put(key3, value3);
+
+		List<String> results = client.range(key1,key3);
+		Assert.assertTrue(results.size()==2);
+		Assert.assertTrue(results.contains(value1.toStringUtf8()));
+		Assert.assertTrue(results.contains(value2.toStringUtf8()));
+		Assert.assertFalse(results.contains(value3.toStringUtf8()));
 	}
 
+	@Test
+	public void testWatch(){
+		final ByteString key1 = ByteString.copyFromUtf8("range1");
+		ByteString value1 = ByteString.copyFromUtf8("good1");
+		final ByteString value2 = ByteString.copyFromUtf8("good2");
+
+		client.put(key1, value1);
+		Assert.assertTrue(true);
+		StreamObserver<WatchResponse> responseObserver=new WatchStreamObserver(){
+			@Override
+			public void onNext(WatchResponse response) {
+				if(response.getCreated()){
+					watchId=response.getWatchId();
+				}else{
+					String key = response.getEventsList().get(0).getKv().getKey().toStringUtf8();
+					String value = response.getEventsList().get(0).getKv().getValue().toStringUtf8();
+				    Assert.assertTrue(value.equals(value2.toStringUtf8()));
+				    Assert.assertTrue(key.equals(key1.toStringUtf8()));
+				}
+			}
+			};
+			
+		client.watch(key1,responseObserver);
+		client.put(key1, value2);
+		Assert.assertTrue(true);
+
+	}
+	
+	
+	@Test
+	public void testWatchPrefix(){
+		final ByteString prefix = ByteString.copyFromUtf8("prefix");
+		final ByteString key1 = ByteString.copyFromUtf8("prefixRange1");
+		ByteString value1 = ByteString.copyFromUtf8("good1");
+		final ByteString value2 = ByteString.copyFromUtf8("good2");
+
+		client.put(key1, value1);
+		Assert.assertTrue(true);
+		StreamObserver<WatchResponse> responseObserver=new WatchStreamObserver(){
+			@Override
+			public void onNext(WatchResponse response) {
+				if(response.getCreated()){
+					watchId=response.getWatchId();
+				}else{
+					String key = response.getEventsList().get(0).getKv().getKey().toStringUtf8();
+					String value = response.getEventsList().get(0).getKv().getValue().toStringUtf8();
+				    Assert.assertTrue(value.equals(value2.toStringUtf8()));
+				    Assert.assertTrue(key.equals(key1.toStringUtf8()));
+				}
+			}
+		};
+			
+		client.watchPrefix(prefix,responseObserver);
+		client.put(key1, value2);
+		Assert.assertTrue(true);
+
+	}
+	
+	@Test
+	public void testLease(){
+		int ttl =5;
+		long leaseId = client.createLease(ttl);
+		Assert.assertTrue(leaseId!=0);
+		ByteString key = ByteString.copyFromUtf8("lease");
+		ByteString value = ByteString.copyFromUtf8("good1");
+		
+		client.put(key, value, leaseId);
+		String result = client.range(key);
+		Assert.assertEquals(value.toStringUtf8(), result);
+		try {
+			Thread.sleep((ttl+1)*1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		String expiredResult = client.range(key);
+		Assert.assertNull(expiredResult);
+		
+		 leaseId = client.createLease(ttl);
+		 
+		 ByteString key2 = ByteString.copyFromUtf8("lease2");
+		 ByteString value2 = ByteString.copyFromUtf8("good2");
+			
+		 client.put(key2, value2, leaseId);
+		 client.keepAliveLease(leaseId);
+		 try {
+				Thread.sleep((ttl+1)*1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			String aliveResult = client.range(key2);
+			Assert.assertEquals(value2.toStringUtf8(), aliveResult);
+	}
+	
+	
 	@After
 	public void end() {
+		
 		try {
 			client.shutdown();
 		} catch (InterruptedException e) {
